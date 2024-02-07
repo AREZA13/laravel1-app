0874b48cd9eb4ae998ce10f7e0e943a3a2f393f8
AREZA13 <arezasoft@icloud.com>
2024-02-07 13:13:46 +0300

    refactored function names and routes. list of type options adding new pet implemented

--- resources/views/add-new-pet.blade.php
+++ resources/views/add-new-pet.blade.php
@@ -11,28 +11,30 @@ use App\DTO\Pet\Pet;
 <form class="max-w-sm mx-auto">
     <p id="helper-text-explanation" class="mt-2 p-8 text-lg text-gray-500 dark:text-gray-500 font-large  "
        style="text-align: center">Add info for new pet.</p>
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900"></label>
-    <input type="email" id="email" aria-describedby="helper-text-explanation"
+
+    <label for="alias" class="block mb-2 text-sm font-medium text-gray-900"></label>
+    <input type="text" id="alias" aria-describedby="helper-text-explanation"
            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
            placeholder="Alias">
 
-    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-500">Select type</label>
-    <select id="countries"
+    <label for="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-500">Select type</label>
+    <select id="type"
+            name="type"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ">
 
-        <option>United States</option>
+        <option value="3">United States</option>
         <option>Canada</option>
     </select>
 
-    <label for="countries" class="c text-sm font-medium text-gray-900 dark:text-gray-500">Select breed</label>
-    <select id="countries"
+    <label for="breed" class="c text-sm font-medium text-gray-900 dark:text-gray-500">Select breed</label>
+    <select id="breed"
+            disabled="disabled"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-
         <option>United States</option>
         <option>Canada</option>
     </select>
 
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Owner Id</label>
     <input type="hidden" id="email" aria-describedby="helper-text-explanation"
            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
            placeholder="name@flowbite.com">
@@ -44,3 +46,28 @@ use App\DTO\Pet\Pet;
        id="pet-button-add">&#129437;&#129451; Add new pet </a>
 </div>
 
+<script>
+    {{--    Functions --}}
+    async function fillPEtTypesIntoSelectOption() {
+        const responseWithTypes = await fetch("/petType", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        const decodedJson = await responseWithTypes.json();
+        decodedJson.forEach((type) => {
+            insertIntoOptionType(type)
+        });
+    }
+
+    function insertIntoOptionType(type) {
+
+        let row = `<option value="${type.id}">${type.title}</option>`
+        document.getElementById("type").innerHTML += row;
+    }
+
+    {{--    Actions --}}
+    document.addEventListener("DOMContentLoaded", fillPEtTypesIntoSelectOption);
+</script>
